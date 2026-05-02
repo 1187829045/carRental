0ef070b2721b2724031c8b4bc8a7bd6da284bc31
liulunbo <liulunbo@gmail.com>
2026-05-03 01:19:41 +0800

    fix

--- cmd/goserver/main.go
+++ cmd/goserver/main.go
@@ -80,24 +80,26 @@ func main() {
 	}
 }
 
-//[GIN-debug] POST   /carRental/rent/checkCustomerExist.action --> carRental/internal/handler.registerBusRoutes.func17 (7 handlers)
-// panic: handlers are already registered for path '/carRental/rent/checkCustomerExist.action'
+// [GIN-debug] POST   /carRental/rent/updateRent.action --> carRental/internal/handler.registerBusRoutes.func16 (7 handlers)
+// [GIN-debug] POST   /carRental/rent/checkRent.action --> carRental/internal/handler.registerBusRoutes.func17 (7 handlers)
+// [GIN-debug] GET    /carRental/rent/loadAllRent.action --> carRental/internal/handler.registerBusRoutes.func18 (7 handlers)
+// panic: handlers are already registered for path '/carRental/rent/loadAllRent.action'
 
 // goroutine 1 [running]:
-// github.com/gin-gonic/gin.(*node).addRoute(0x7ff65d360b03?, {0xc000402870, 0x29}, {0xc00017afc0, 0x7, 0x7})
+// github.com/gin-gonic/gin.(*node).addRoute(0x7ff6cc6ff7dd?, {0xc0003f8960, 0x22}, {0xc0003f1040, 0x7, 0x7})
 //         C:/Users/11878/go/pkg/mod/github.com/gin-gonic/gin@v1.12.0/tree.go:243 +0x6e8
-// github.com/gin-gonic/gin.(*Engine).addRoute(0xc000106540, {0x7ff65d360b03, 0x4}, {0xc000402870, 0x29}, {0xc00017afc0, 0x7, 0x7})
+// github.com/gin-gonic/gin.(*Engine).addRoute(0xc0003a0380, {0x7ff6cc6ff7dd, 0x3}, {0xc0003f8960, 0x22}, {0xc0003f1040, 0x7, 0x7})
 //         C:/Users/11878/go/pkg/mod/github.com/gin-gonic/gin@v1.12.0/gin.go:377 +0x25f
-// github.com/gin-gonic/gin.(*RouterGroup).handle(0xc00014e780, {0x7ff65d360b03, 0x4}, {0x7ff65d393596?, 0xc0001202f0?}, {0xc000120548, 0x1, 0xc0001202f0?})
+// github.com/gin-gonic/gin.(*RouterGroup).handle(0xc0003c0780, {0x7ff6cc6ff7dd, 0x3}, {0x7ff6cc729b4c?, 0xc0003982f0?}, {0xc000398558, 0x1, 0xc0003982f0?})
 //         C:/Users/11878/go/pkg/mod/github.com/gin-gonic/gin@v1.12.0/routergroup.go:89 +0x13e
-// github.com/gin-gonic/gin.(*RouterGroup).POST(...)
-//         C:/Users/11878/go/pkg/mod/github.com/gin-gonic/gin@v1.12.0/routergroup.go:112
-// carRental/internal/handler.registerBusRoutes(0xc00014e780, {{{0x7ff65d362613, 0x5}, {0x7ff65d372846, 0xa}, {0xc0001b0680, 0x34}, {0xc0001ccd80, 0x2e}, {0xc00021e420, ...}, ...}, ...})
-//         E:/go_project/carRental/internal/handler/modules.go:682 +0xa0a
-// carRental/internal/handler.registerModules(0xc00014e780, {{{0x7ff65d362613, 0x5}, {0x7ff65d372846, 0xa}, {0xc0001b0680, 0x34}, {0xc0001ccd80, 0x2e}, {0xc00021e420, ...}, ...}, ...})
+// github.com/gin-gonic/gin.(*RouterGroup).GET(...)
+//         C:/Users/11878/go/pkg/mod/github.com/gin-gonic/gin@v1.12.0/routergroup.go:117
+// carRental/internal/handler.registerBusRoutes(0xc0003c0780, {{{0x7ff6cc702622, 0x5}, {0x7ff6cc712862, 0xa}, {0xc0000328c0, 0x34}, {0xc000031140, 0x2e}, {0xc00003a480, ...}, ...}, ...})
+//         E:/go_project/carRental/internal/handler/modules.go:694 +0xa98
+// carRental/internal/handler.registerModules(0xc0003c0780, {{{0x7ff6cc702622, 0x5}, {0x7ff6cc712862, 0xa}, {0xc0000328c0, 0x34}, {0xc000031140, 0x2e}, {0xc00003a480, ...}, ...}, ...})
 //         E:/go_project/carRental/internal/handler/modules.go:29 +0xe5
-// carRental/internal/handler.NewRouter({{{0x7ff65d362613, 0x5}, {0x7ff65d372846, 0xa}, {0xc0001b0680, 0x34}, {0xc0001ccd80, 0x2e}, {0xc00021e420, 0x54}, ...}, ...})
-//         E:/go_project/carRental/internal/handler/router.go:354 +0x2c89
+// carRental/internal/handler.NewRouter({{{0x7ff6cc702622, 0x5}, {0x7ff6cc712862, 0xa}, {0xc0000328c0, 0x34}, {0xc000031140, 0x2e}, {0xc00003a480, 0x54}, ...}, ...})
+//         E:/go_project/carRental/internal/handler/router.go:360 +0x2ce9
 // main.main()
 //         E:/go_project/carRental/cmd/goserver/main.go:60 +0x758
 // exit status 2
